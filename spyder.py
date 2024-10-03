57df298455e66ec7a7c9da9f15aff0ff7f1e5839
winston <winstonuber21@gmail.com>
2024-10-04 03:50:51 +0800

    sleepy commit

--- spyder.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  4 00:35:31 2024
-
-@author: winst
-"""
-
-for x in range(1,11):
-    print(x)
--- spyder/Python Assignment1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  4 01:52:21 2024
-
-@author: winst
-"""
-
-for x in range(1,11):
-  print(x)
